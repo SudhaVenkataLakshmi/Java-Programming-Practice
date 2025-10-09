900f566b80e30048b70b0176cd48c98cf2e98cd9
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-10-09 19:57:08 +0530

    Initial commit

--- InstanceVariable.java
+++ InstanceVariable.java
@@ -2,6 +2,8 @@ public class InstanceVariable {
     byte b;
     float f;
     char c;
+    double d;
+    String str;
     public static void main(String args[]){
 
         InstanceVariable obj1 = new InstanceVariable();
@@ -16,7 +18,13 @@ public class InstanceVariable {
 
         InstanceVariable obj3 = new InstanceVariable();
         System.out.println("The Default Value of char is:"+obj3.c);   
+        obj3.c = 's';
+        System.out.println("The Value of char is: "+obj3.c);
 
+        InstanceVariable obj4 = new InstanceVariable();
+        System.out.println("The Default Value of double is: "+obj4.d);
+        obj4.d = 34.4674643;
+        System.out.println("The Value of double is: "+obj4.d);
 
     }
 }
