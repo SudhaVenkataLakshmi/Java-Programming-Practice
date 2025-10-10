85a0fea7cc596626bb3556a1945963d0a0781cbe
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-10-10 15:53:08 +0530

    Highest among 3

--- Ternary.java
+++ Ternary.java
@@ -1,6 +1,6 @@
 public class Ternary {
     public static void main (String args[]){
-        int a = 10, b = 40, c = 60;
+        int a = 50, b = 100, c = 60;
         int res = ((a>b)?(a>c?a:c):(b>c?b:c));
         System.out.println("Highest Value: "+res);
     }
