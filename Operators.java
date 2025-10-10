d8102c8c0aac484a62f40711b6138d6632c0f23c
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-10-10 14:21:07 +0530

    Initial commit

--- Operators.java
+++ Operators.java
@@ -2,10 +2,23 @@ public class Operators {
     public static void main(String args[]){
         int a = 40;
         int b = 60;
-        System.out.println("Sum of Given Numbers: "+(a+b));
-        System.out.println("Difference of Given Numbers: "+ -((a-b)));
-        System.out.println("Product of Given Numbers:"+(a*b));
-        System.out.println("Division of Given Numbers: "+(b/a));
-        System.out.println("Modulo Division of Given Numbers: "+(b%a));
+
+        //Arithmatic Operators
+
+        // System.out.println("Sum of Given Numbers: "+(a+b));
+        // System.out.println("Difference of Given Numbers: "+ -((a-b)));
+        // System.out.println("Product of Given Numbers:"+(a*b));
+        // System.out.println("Division of Given Numbers: "+(b/a));
+        // System.out.println("Modulo Division of Given Numbers: "+(b%a));
+
+        //Relational Operators
+
+        System.out.println("(a==b):"+(a==b));
+        System.out.println("(a<b):"+(a<b));
+        System.out.println("(a>b):"+(a>b));
+        System.out.println("(a<=b):"+(a<=b));
+        System.out.println("(a>=b):"+(a>=b));
+        System.out.println("(a!=b):"+(a!=b));
+
     }
 }
