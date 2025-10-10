65c1fd034a1725ef7cc1c5ce4b58dad538bc00cd
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-10-10 14:17:28 +0530

    Initial commit

---
+++ Operators.java
@@ -0,0 +1,11 @@
+public class Operators {
+    public static void main(String args[]){
+        int a = 40;
+        int b = 60;
+        System.out.println("Sum of Given Numbers: "+(a+b));
+        System.out.println("Difference of Given Numbers: "+ -((a-b)));
+        System.out.println("Product of Given Numbers:"+(a*b));
+        System.out.println("Division of Given Numbers: "+(b/a));
+        System.out.println("Modulo Division of Given Numbers: "+(b%a));
+    }
+}
