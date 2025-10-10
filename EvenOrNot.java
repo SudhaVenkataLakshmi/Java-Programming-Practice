f21adb846991a6a1d81771278179d808b8812a29
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-10-10 15:30:28 +0530

    Even Or Not

--- EvenOrNot.java
+++ EvenOrNot.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-public class PrimeOrNot {
+public class EvenOrNot {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter any Value: ");
         int num = sc.nextInt();
         if(num % 2 == 0){
-            System.out.println("Given Value is Prime.");
+            System.out.println("Given Value is Even.");
         }else{
-            System.out.println("Given Value is not Prime.");
+            System.out.println("Given Value is not Even.");
         }
     }
 }
--- Operators.java
+++ Operators.java
@@ -5,45 +5,49 @@ public class Operators {
 
         //Arithmatic Operators
 
-        System.out.println("Sum of Given Numbers: "+(a+b));
-        System.out.println("Difference of Given Numbers: "+ -((a-b)));
-        System.out.println("Product of Given Numbers:"+(a*b));
-        System.out.println("Division of Given Numbers: "+(b/a));
-        System.out.println("Modulo Division of Given Numbers: "+(b%a));
+        // System.out.println("Sum of Given Numbers: "+(a+b));
+        // System.out.println("Difference of Given Numbers: "+ -((a-b)));
+        // System.out.println("Product of Given Numbers:"+(a*b));
+        // System.out.println("Division of Given Numbers: "+(b/a));
+        // System.out.println("Modulo Division of Given Numbers: "+(b%a));
 
         //Relational Operators
 
-        System.out.println("(a==b):"+(a==b));
-        System.out.println("(a<b):"+(a<b));
-        System.out.println("(a>b):"+(a>b));
-        System.out.println("(a<=b):"+(a<=b));
-        System.out.println("(a>=b):"+(a>=b));
-        System.out.println("(a!=b):"+(a!=b));
+        // System.out.println("(a==b):"+(a==b));
+        // System.out.println("(a<b):"+(a<b));
+        // System.out.println("(a>b):"+(a>b));
+        // System.out.println("(a<=b):"+(a<=b));
+        // System.out.println("(a>=b):"+(a>=b));
+        // System.out.println("(a!=b):"+(a!=b));
 
         //Logical Operators
 
-        System.out.println((a!=b) && (a>b));
-        System.out.println((a!=b) && (a<b));
-        System.out.println((a==b) || (a<b));
-        System.out.println((a!=10) || (b>a));
-        System.out.println(a!=b);
-        System.out.println(a==b);
+        // System.out.println((a!=b) && (a>b));
+        // System.out.println((a!=b) && (a<b));
+        // System.out.println((a==b) || (a<b));
+        // System.out.println((a!=10) || (b>a));
+        // System.out.println(a!=b);
+        // System.out.println(a==b);
 
         //Assignment Operators
 
-        System.out.println("(a+=3): "+(a+=3));
-        System.out.println("(a-=2): "+(a-=2));
-        System.out.println("(a*=4): "+(a*=4));
-        System.out.println("(a/=2): "+(a/=2));
-        System.out.println("(a%=2): "+(a%=2));
+        // System.out.println("(a+=3): "+(a+=3));
+        // System.out.println("(a-=2): "+(a-=2));
+        // System.out.println("(a*=4): "+(a*=4));
+        // System.out.println("(a/=2): "+(a/=2));
+        // System.out.println("(a%=2): "+(a%=2));
 
         //Increment & Decrement Operators
 
-        System.out.println("The Value of a is: "+a);
-        System.out.println("(++a): "+(++a));
-        System.out.println("(a++): "+(a++));
-        System.out.println("(--a): "+(--a));
-        System.out.println("(a--): "+(a--));
-        System.out.println("The Value of a is: "+a);
+        // System.out.println("The Value of a is: "+a);
+        // System.out.println("(++a): "+(++a));
+        // System.out.println("(a++): "+(a++));
+        // System.out.println("(--a): "+(--a));
+        // System.out.println("(a--): "+(a--));
+        // System.out.println("The Value of a is: "+a);
+
+        //Ternary Operator
+
+        
     }
 }
