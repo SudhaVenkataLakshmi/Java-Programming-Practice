72c7d24e2e041d59c0d41d788732b0ec2a43932f
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-10-16 14:36:40 +0530

    Bitwise Operations

--- Bitwise.java
+++ Bitwise.java
@@ -1,7 +1,7 @@
 public class Bitwise {
     public static void main(String[] args){
-        int a = 20;
-        int b = 10;
+        int a = 10;
+        int b = 5;
         System.out.println("a&b: "+(a&b));
         System.out.println("a|b: "+(a|b));
         System.out.println("a^b: "+(a^b));
