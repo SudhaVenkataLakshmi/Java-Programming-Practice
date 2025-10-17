4bc6dce3bc9b05750d3af2af16a440be96d62285
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-10-17 14:29:45 +0530

    Print even Numbers from given range

---
+++ ForLoop.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ForLoop {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Starting Value: ");
+        int start = sc.nextInt();
+        System.out.println("Enter Ending Value: ");
+        int end = sc.nextInt();
+        System.out.println("Values between " +start+ " and " +end+ " are: ");
+        for(int i = start; i < end; i++){
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
