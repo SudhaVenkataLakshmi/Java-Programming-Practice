2814615de0e51d25cdce73ccaf5810f603c1b2d8
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-10-23 19:30:24 +0530

    Checks Eligibility for Vote

---
+++ VoteEligible.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class VoteEligible {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your Age:");
+        int age = sc.nextInt();
+        if(age >= 18){
+            System.out.println("You are Eligible for Vote.");
+        }else
+        System.out.println("You are not Eligible for Vote.");
+    }
+}
