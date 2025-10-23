a18b21517db50b6b4ccba2f18370b032d3dfc0ca
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-10-23 20:43:14 +0530

    Example

--- IfElse.java
+++ IfElse.java
@@ -1,6 +1,6 @@
 public class IfElse {
     public static void main(String[] args){
-        int age = 12;
+        int age = 35;
         if(age>=18){
             System.out.println("Eligible for Vote.");
         }else{
