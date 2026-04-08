d86e370709a63f4f877422ec7a42220ab3d8011d
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-04-08 19:54:07 +0530

    Do While problem

--- DoWhile.java
+++ DoWhile.java
@@ -1,9 +1,9 @@
 public class DoWhile {
     public static void main(String[] args){
-        int i = 1;
+        int i = 5;
         do{
             System.out.println(i);
             i++;
-        }while(i < 1);
+        }while(i < 10);
     }
 }
