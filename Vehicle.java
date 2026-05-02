a325247807ac3c3c9c96dd8ec19c5c065e7cdd45
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-02 20:26:48 +0530

    Vehicle program

---
+++ Vehicle.java
@@ -0,0 +1,19 @@
+abstract class Vehicle {
+    abstract void start();
+}
+class Car extends Vehicle {
+    void start() {
+        System.out.println("Car starts with Key");
+    }
+}
+class Bike extends Vehicle {
+    void start() {
+        System.out.println("Bike starts with Kick");
+    }
+    public static void main(String[] args) {
+        Vehicle v1 = new Car();
+        Vehicle v2 = new Bike();
+        v1.start();
+        v2.start();
+    }
+}
