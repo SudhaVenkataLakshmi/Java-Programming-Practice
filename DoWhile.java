39cd0a2e51da596d5973581abb320f69c532bd23
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-02 20:28:30 +0530

    DoWhile Practice

--- DoWhile.java
+++ DoWhile.java
@@ -1,9 +1,9 @@
 public class DoWhile {
     public static void main(String[] args){
-        int i = 10;
+        int i = 1;
         do{
             System.out.println(i);
             i++;
-        }while(i < 50);
+        }while(i < 10);
     }
 }
