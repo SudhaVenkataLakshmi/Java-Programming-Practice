f46a031c0f3001c3c4a6643b258b600356f6f4cd
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-02 14:58:28 +0530

    Do While concept

--- DoWhile.java
+++ DoWhile.java
@@ -1,9 +1,9 @@
 public class DoWhile {
     public static void main(String[] args){
-        int i = 5;
+        int i = 10;
         do{
             System.out.println(i);
             i++;
-        }while(i < 10);
+        }while(i < 50);
     }
 }
