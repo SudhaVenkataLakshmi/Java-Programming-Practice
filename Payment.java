9cff3260f12636042a3ec8f7dd94a5b860a96896
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-03 16:54:25 +0530

    Payment Details

--- Payment.java
+++ Payment.java
@@ -13,7 +13,7 @@ class Card implements Payment {
     public static void main(String[] args) {
         Payment p1 = new UPI();
         Payment p2 = new Card();
-        p1.pay(4000);
-        p2.pay(12000);
+        p1.pay(3000);
+        p2.pay(15000);
     }
 }
