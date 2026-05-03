dc015b7646cdfe9606f8b960eaea69858bd53a57
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-03 16:39:44 +0530

    Payment Details

---
+++ Payment.java
@@ -0,0 +1,19 @@
+interface Payment {
+    void pay(double amount);
+}
+class UPI implements Payment {
+    public void pay(double amount){
+        System.out.println("Paid via UPI: " +amount);
+    }
+}
+class Card implements Payment {
+    public void pay(double amount){
+        System.out.println("Paid via Card: " +amount);
+    }
+    public static void main(String[] args) {
+        Payment p1 = new UPI();
+        Payment p2 = new Card();
+        p1.pay(5000);
+        p2.pay(10000);
+    }
+}
