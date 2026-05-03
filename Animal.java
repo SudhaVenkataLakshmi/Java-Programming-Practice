c98768ab40de80bec32be4d8cfecb6073e283786
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-03 16:31:26 +0530

    Constructor topic

--- Animal.java
+++ Animal.java
@@ -6,7 +6,7 @@ public class Animal {
 class Dog extends Animal {
     Dog() {
         super();
-        System.out.println("Dog Constructor");
+        System.out.println("Dog was Barking");
     }
     public static void main(String[] args) {
         new Dog();
