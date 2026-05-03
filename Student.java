899f6edbeb3af2e7980ded8b6521aeb62a7cdf64
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-03 16:28:07 +0530

    Student Details

--- Student.java
+++ Student.java
@@ -14,7 +14,7 @@ public class Student {
     }
     public static void main(String[] args) {
         // Student s1 = new Student();
-        Student s2 = new Student("Sushma", 22);
+        Student s2 = new Student("Sandhya", 23);
         // s1.display();
         s2.display();
     }
