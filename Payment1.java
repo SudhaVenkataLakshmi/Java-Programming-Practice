78b4124c93890b4e30b90cbca5528bdaaa3a6590
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-04 15:19:00 +0530

    Use of Interface

---
+++ Payment1.java
@@ -0,0 +1,16 @@
+interface Payment1 {
+    void pay(double amount);
+        default void receipt() {
+            System.out.println("Payment Successful");
+    }
+}
+class UPI implements Payment1{
+    public void pay(double amount){
+        System.out.println("UPI Payment: " +amount);
+    }
+    public static void main(String[] args) {
+        UPI u = new UPI();
+        u.pay(500);
+        u.receipt();
+    }
+}
