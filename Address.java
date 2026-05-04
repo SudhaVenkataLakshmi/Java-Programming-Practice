1a40a9623a7b61e0c5c0d248da2495f14f008f55
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-04 15:25:23 +0530

    Student address details

--- Address.java
+++ Address.java
@@ -15,7 +15,7 @@ class Student {
         System.out.println(name+ " lives in " +address.city);
     }
     public static void main(String[] args){
-        Student s = new Student("Sushma", "Kakinada");
+        Student s = new Student("Sandhya", "Tuni");
         s.display();
     }
 }
