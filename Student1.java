c5b9ebf798e2e6d7f68d29d77101862d7fe8fbf6
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-04 15:45:08 +0530

    Counts the no.of Students

--- Student1.java
+++ Student1.java
@@ -6,9 +6,6 @@ public class Student1 {
     public static void main(String[] args){
         new Student1();
         new Student1();
-        new Student1();
-        new Student1();
-        new Student1();
         System.out.println("Total Students: " +count);
     }
 }
