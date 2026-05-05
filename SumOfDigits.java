e873e00136da41bd71a475a8f143bd3c58d9d0dd
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-05 20:55:53 +0530

    Gives Sum of Given number

--- SumOfDigits.java
+++ SumOfDigits.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class SumOfDigits {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter any Number: ");
+        System.out.println("Enter Number: ");
         int num = sc.nextInt();
         int sum = 0;
         while(num != 0){
