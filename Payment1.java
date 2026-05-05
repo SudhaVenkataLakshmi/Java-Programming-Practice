493a9d100b0ece16959f4c87a0d9ae62f1f9971b
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-05 20:41:17 +0530

    Paymnet Details

--- Payment1.java
+++ Payment1.java
@@ -10,7 +10,7 @@ class UPI implements Payment1{
     }
     public static void main(String[] args) {
         UPI u = new UPI();
-        u.pay(1800);
+        u.pay(2500);
         u.receipt();
     }
 }
