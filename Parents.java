ce4f4ba82e0eb4664d5bf32db8b67bffa43907ae
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-08 21:22:33 +0530

    Method Overriding

--- Parents.java
+++ Parents.java
@@ -1,12 +1,12 @@
 public class Parents {
     void show() {
-        System.out.println("Parent Content");
+        System.out.println("Parent Class");
     }
 }
 class Child extends Parents{
     @Override
     void show() {
-        System.out.println("Child Content");
+        System.out.println("Child Class");
     }
     public static void main(String[] args) {
         Parents obj = new Child();
