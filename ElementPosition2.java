7b8b4508b283829de6af9d5d082fe1979f057195
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-11 15:53:29 +0530

    Findind last occurance of Element in Array

---
+++ ElementPosition2.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class ElementPosition2 {
+    public static int firstPosition(int[] nums, int target) 
+	{
+        int low = 0, high = nums.length - 1;
+        int ans = -1;
+
+        while (low <= high) 
+		{
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) 
+			{
+                ans = mid;
+                low = mid + 1; 				// move left
+            }
+            else if (nums[mid] < target) 
+			{
+                low = mid + 1;
+            }
+            else 
+			{
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int target = sc.nextInt();
+
+        int result = firstPosition(nums, target);
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
