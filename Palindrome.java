73451ff37ffd3ebf830c473f9d286b9cf33b0c69
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-12 21:25:05 +0530

    Checks Palindrome or not

--- Palindrome.java
+++ Palindrome.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter any Number: ");
+        System.out.println("Enter Number: ");
         int number = sc.nextInt();
         int temp = number;
         int rev = 0;
