ded376de5c893fee263a4142f5b67eb1b4f01f15
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-21 20:22:08 +0530

    Payment Details

--- Payment1.java
+++ Payment1.java
@@ -10,7 +10,7 @@ class UPI implements Payment1{
     }
     public static void main(String[] args) {
         UPI u = new UPI();
-        u.pay(2000);
+        u.pay(1800);
         u.receipt();
     }
 }
